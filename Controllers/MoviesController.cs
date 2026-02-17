19b4bb46331930a6f49bea0b1cab76bcad346fe1
Marian Bosnea <44442182+marian-bosnea@users.noreply.github.com>
2026-02-17 19:41:10 +0200

    Added initial project

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+       //GET: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            return View(movie);
+        }
+    }
+}
